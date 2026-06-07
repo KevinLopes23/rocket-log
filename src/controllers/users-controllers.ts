58cd02810d20fd8eee1de9557cbcfc231401584b
Kevin Lopes <kevinlopes2304@gmail.com>
2026-06-07 11:45:46 -0300

    Add validation schema for user creation using Zod

--- src/controllers/users-controllers.ts
+++ src/controllers/users-controllers.ts
@@ -1,10 +1,18 @@
 import { Request, Response } from "express";
+import { z } from "zod";
 
 class UsersController {
   create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string().min(2).max(100),
+      email: z.string().email(),
+      password: z.string().min(6),
+    });
+
+    const { name, email, password } = bodySchema.parse(request.body);
+
     return response.json({ message: "ok" });
   }
 }
-
 
 export { UsersController };
