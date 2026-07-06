cf377e09ea76078558e009929ba270f4ac45e0d8
Kevin Lopes <kevinlopes2304@gmail.com>
2026-07-06 20:53:37 -0300

    Refactor UsersController tests to define email variable once and reuse it

--- src/tests/users-controller.test.ts
+++ src/tests/users-controller.test.ts
@@ -6,6 +6,7 @@ import { prisma } from "../database/prisma";
 
 describe("UsersController", () => {
   let user_id: string;
+  const email = `testuser-${Date.now()}@example.com`;
 
   afterAll(async () => {
     await prisma.user.delete({
@@ -16,8 +17,6 @@ describe("UsersController", () => {
   });
 
   it("should create a new user successfully", async () => {
-    const email = `testuser-${Date.now()}@example.com`;
-
     const response = await request(app).post("/users").send({
       name: "Test User",
       email,
@@ -30,4 +29,25 @@ describe("UsersController", () => {
 
     user_id = response.body.id;
   });
+
+  it("should throw an error if user with same email already exists", async () => {
+    const response = await request(app).post("/users").send({
+      name: "Duplicate User",
+      email,
+      password: "password123",
+    });
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe("User with same email already exists");
+  });
+
+  it("should throw a validation error if email is invalid", async () =>{
+    const response = await request(app).post("/users").send({
+      name: "Test User",
+      email: "invalid-email",
+      password: "password123",
+    })
+
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe("validation error");
+  })
 });
